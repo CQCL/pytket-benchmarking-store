07dfdd1979885db91f3ae6ca79fe992e82e0d28c
danmills0 <daniel.mills@cambridgequantum.com>
2024-01-20 13:46:33 +0000

    Fix test

--- tests/all_can_upload.py
+++ tests/all_can_upload.py
@@ -1,4 +1,4 @@
-from pytket_benchmarking.circuit_suite.circuit_suite_manager import CircuitSuiteManager
+from pytket_benchmarking.compiler_benchmarking.circuit_suite_manager import CircuitSuiteManager
 from pytket_benchmarking.utils.storage_manager import LocalStorage
 from pathlib import Path
 
@@ -7,5 +7,7 @@ for directory_path in Path("benchmarking_circuits").iterdir():
     circuit_suite_mgr = CircuitSuiteManager(
         storage_manager=storage_manager
     )
-    if not circuit_suite_mgr.can_upload:
+    if (not circuit_suite_mgr.can_upload) or (len(circuit_suite_mgr.df) == 0):
         raise Exception(f"{directory_path} is not a valid circuit suite.")
+
+    print(f'{directory_path} contains a circuit suite of {len(circuit_suite_mgr.df)} circuits')
